5e7dcff0afb3059578a22bdee209e5f6569c02af
Alex <alex.sircu@gmail.com>
2021-04-21 23:44:19 +0200

    Installation

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+    <title>Laravel</title>
+
+  </head>
+  <body>
+    <header>
+      <h2>Questa sezione sarà dedicata all'header</h2>
+    </header>
+
+    <main>
+      <h1>Questa sezione sarà dedicata ad una sorta di jumbotron</h1>
+    </main>
+
+    <footer>
+
+    </footer>
+  </body>
+</html>
